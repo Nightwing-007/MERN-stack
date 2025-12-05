d4c8a48028e71ccbc88583af3705d369fa4466b2
Deepakraj <deepakraj.s2024cse@sece.ac.in>
2025-12-05 11:30:33 +0530

    The Guess folder now has full functionality to use with HTML, CSS, and JS

--- HTML/js/dom/dom.js
+++ HTML/js/dom/dom.js
@@ -1,4 +1,4 @@
-//1. Tag Selector
+/*//1. Tag Selector
 let heading1 = document.getElementsByTagName("h1");
 //HTML Collection Array
 console.log(heading1[1]);
@@ -21,6 +21,48 @@ console.log(sel);
 //5. Query Selector All
 let d = document.querySelectorAll("#b");
 //NodeList Array
-console.log(d);
+console.log(d);*/
 
-//Read and Write operations
+/*
+//Read and Write operation
+let head = document.querySelector("p");
+//Read operation
+console.log(head.textContent);
+
+//Write operation
+head.textContent = "This is the new paragraph content.";
+
+let div = document.querySelector("div");
+div.innerHTML = "<p>Hello from para</p>";*/
+
+// add/remove/toggle class
+let head = document.querySelector("h1");
+console.log(head);
+
+head.classList.add("color");
+head.classList.remove("border");
+head.classList.toggle("background");
+
+//Styling
+let p = document.querySelector("p");
+p.style.color = "red";
+p.style.backgroundColor = "green";
+
+//Creating Element
+let ul = document.createElement("ul");
+let li = document.createElement("li");
+li.textContent = "Item 1";
+let li1 = document.createElement("li");
+li1.textContent = "Item 2";
+ul.appendChild(li);
+ul.appendChild(li1);
+
+let arr = ["apple", "banana", "mango"];
+
+arr.forEach((el) => {
+    let li = document.createElement("li");
+    li.textContent = el;
+    ul.appendChild(li);
+});
+
+document.body.appendChild(ul);
